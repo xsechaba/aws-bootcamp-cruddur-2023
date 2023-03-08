8ec3ea196a02d2cd21b2b74d2d96d461ad7502f7
xsechaba <xsechaba@gmail.com>
2023-03-08 05:28:45 +0000

    fixes and updates

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -5,14 +5,14 @@ tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
   def run():
-    with tracer.start_as_current_span("home-activities-mock-data"):
+    with tracer.start_as_current_span("home-activites-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat())
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
+        'message': 'Cloud is very fun!',
         'created_at': (now - timedelta(days=2)).isoformat(),
         'expires_at': (now + timedelta(days=5)).isoformat(),
         'likes_count': 5,
